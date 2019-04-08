7e3b6d8006c453956309d3fbc00477b493257c6e
Açal Habi <habi_a@etna-alternance.net>
2019-04-08 16:18:23 +0200

    add migrations cooperative tours

---
+++ api/database/migrations/2019_04_08_140528_create_tour_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTourTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tour', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('type');
+            $table->foreign('cooperative_id')->references('id')->on('cooperative');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tour');
+    }
+}
